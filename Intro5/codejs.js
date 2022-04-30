698cf0e25655a6bd5e1240ba78a74172467bdf27
Federico Irarrazaval <fedeirar@gmail.com>
2022-04-30 10:10:33 -0300

    video 7

---
+++ Intro5/codejs.js
@@ -0,0 +1,16 @@
+/* 
+OPERADORES INCREMENTO / DECREMENTO
+
++= : incrementa en x el valor de una variable, ejemplo=  Record+=10
+
+-= : decrement en x el valor de una variable, ejemplo=  Record-=7
+
+*=  : multiplica en x el valor de una variable, ejemplo=  Record*=3
+
+/= : divide en x el valor de una variable, ejemplo=  Record/=4
+
+++ : incrementa en 1 el valor de una variable, ejemplo=  Record++
+
+-- : decrementa en 1 el valor de una variable, ejemplo=  Record--
+*/
+
