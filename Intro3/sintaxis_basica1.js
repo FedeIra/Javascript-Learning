749336722b05d4c4e054aac75d771392d248b81b
Federico Irarrazaval <fedeirar@gmail.com>
2022-04-29 23:42:04 -0300

    video 4

---
+++ Intro3/sintaxis_basica1.js
@@ -0,0 +1,50 @@
+/* SENTENCIAS 
+
+ con el punto y coma termina la sentencia. Aparte te puede tirar error. Sirve para ver donde termina la sentencia. */
+
+alert ("Hola alumnos");
+
+document.write("Hola alumnos");
+
+/* Funciones predefinidas: las que vienen con el lenguaje. Biblioteca con funciones. Las funciones siempre llevan unos parentesus
+
+Hay funciones propias tambien: las q armas vos.*/
+
+alert();
+document.write();
+isNan();
+
+/* tipos de datos: 
+numeros o interdigit (int), 
+strings: texto. Siempre entre comillas. Comillas dobles o simples. Es lo mismo en general. Pero cambia con comillas embutidas o anidadas.
+booleans: True o False
+
+VARIABLE: espacio en la memoria del ordenador donde se almacena un valor. Puede ir cambiando.
+
+le pongo var y nombre de la variable y luego de doy valor
+
+el nombre de la variable tiene que comenzar con letra, simbolo $ o _
+Pueden contener solo letras, numeros, $ y _
+son case sensitive: cambia el tema de mayuscula y minuscula
+Hay palabras reservables con las q no se puede nombrar variables: como alert
+Se recomienda que sean descriptivos
+*/
+
+// nombres de variables
+var nombre_variable0 = 0;
+var $nombre_variable1 = 1;
+var _nombre_variable2 = 2;
+
+// iniciar una variable
+var nombre_variable3;
+nombre_variable3= 3;
+
+var nombre_variable4 = 4;
+
+// Declaracion de varias variables en la misma linea
+var nombre_variable5, nombre_variable6, nombre_variable7 
+nombre_variable5 = 5; 
+nombre_variable6 = 6; 
+nombre_variable7 = 7;
+
+var nombre_variable8 = 8, nombre_variable9 = True, nombre_variable10 = "Fede"
