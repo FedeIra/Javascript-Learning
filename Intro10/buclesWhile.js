9b1e1d6d008ef6cd429eaf8614d3788ba0cab03a
Federico Irarrazaval <fedeirar@gmail.com>
2022-05-01 18:43:04 -0300

    video 16

---
+++ Intro10/buclesWhile.js
@@ -0,0 +1,20 @@
+/* 
+Los bucles tambien hace control de flujo. Especificamente, hacen que el flujo vuelva para atras y se repita en cierta forma
+
+Hay dos tipos de bucles: los determinados e indeterminados. Los dos repiten codigo. Repiten x veces unas cuantas de las lineas.
+
+Los determinados sabemos cuantas veces van a repetir esas lineas. For (for)
+
+Los indeterminados no sabemos a priori cuantas veces van a repetir ciertas lineas (While y Do While)
+
+while = mientras
+
+While (condicion) {
+    //codigo a repetir mientas la condicion del bucle sea cierta
+}
+// continuacion del programa
+
+Si la condicion siempre es verdad entonces se va a repetir infinitamente el bucle.
+
+En un momento la condicion deja de ser verdad para ser mentira y continua la ejecucion del programa.
+*/
