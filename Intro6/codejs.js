6e18cbd65aa98099ee77848423f8e2338147b8e2
Federico Irarrazaval <fedeirar@gmail.com>
2022-05-01 00:45:34 -0300

    video 7

--- Intro6/codejs.js
+++ Intro6/codejs.js
@@ -16,13 +16,13 @@ Como un archivador con varias pestanias y cada pestania es un valor. O como una 
 
 */
 
-var articles = ["snickers", "trouser", "shirt", True, 0, "sockets"]
+var articles = ["snickers", "trouser", "shirt", true, 0, "sockets"]
 
-var articles2 = new Array("jacket", 1.56, False) // estas dos son las formas de crear arrays o conjunto de valores. Es parecido a las listas de python
+var articles2 = new Array("jacket", 1.56, false) // estas dos son las formas de crear arrays o conjunto de valores. Es parecido a las listas de python
 
 // el primer elemento de un array arranca desde 0. Los podes usar con el [0] , [1], etc.
 
-alert(articles[2]); // con este agarras el tercer articulo del array de articulos, es decir, shirt
+// alert(articles[2]); // con este agarras el tercer articulo del array de articulos, es decir, shirt
 
 /* 
 PROPIEDAD Y METODOS DEL ARRAY IMPORTANTES
@@ -32,7 +32,26 @@ Al ser un lenguaje orientada a metodos (javascript), tiene propiedades y metodos
 Propiedad lenght: cuanto es la longitud o elementos que tiene un array
 */
 
-articles.length
+articles.length = 4;
 
 
 console.log(articles.length);
+
+var articles3 = new Array("jacket", 1.56, false)
+
+// para agregar un elemento mas esta el metodo push. Los agrega al final
+
+articles3.push("hat")
+
+// el metodo unshift hace lo mismo q el push pero los agrega al ppio
+
+articles3.unshift(4)
+
+console.log(articles3) // te va a imprimir = [ 4, 'jacket', 1.56, false, 'hat' ]
+
+// metodo shift es para eliminar elementos al principio y metodo pop para eliminar al final
+
+articles3.shift()
+articles3.pop()
+
+console.log(articles3) // es igual a = [ 'jacket', 1.56, false ]
