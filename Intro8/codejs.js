663d6a620e88b190573c3f063d966909537fe06a
Federico Irarrazaval <fedeirar@gmail.com>
2022-05-01 13:55:05 -0300

    video 12

---
+++ Intro8/codejs.js
@@ -0,0 +1,43 @@
+/* 
+----------------------ESTRUCTURA DE CONTROL DE FLUJOS:-------------------
+
+la finalidad es de dotar de cierta inteligencia a las paginas web
+
+Modifican el flujo de ejecucion del programa. Va de arriba a abajo. Estas modifican el flujo lineal para abajo saltando a otros lados.
+
+Condicionales y bucles
+
+
+-------------------------CONDICIONALES: ---------------------- 
+
+el codigo toma funciones por condiciones. Ejemplo:cuando el usuario llena un formulario. 
+
+Le planteamos si el usuario llena la casilla de nombre que la envie. como ejemplo
+
+Se puede agregar varias condiciones a una sola
+
+Se hace con el if, seguida de () que es la condicion, seguido de {} que es lo q va a hacer si se cumple la condicion
+
+El else tambien esta pero no tiene pq estar agregado. Es para el caso de q no se cumpla la condicion que haga algo.
+
+Para chequear si se cumple la condicion tenemos para usar los OPERADORES DE COMPARACION
+
+
+-----------------OPERADORES DE COMPARACION:----------------------
+
+==  Igual que..
+!= Diferente que..
+> mayor que ..
+< menor que..
+>= mayor o igual que..
+<= menor o igual que
+=== estrictamente igual que (comprueba tambien tipos, es decir, string, booleano o numero)
+!== estrictamente diferente que... (comprueba tipos)
+
+Aparte contamos con los OPERADORES LOGICOS
+
+&&  Y logico (en python es el AND) se tienen q cumplir las dos condiciones
+|| O logico (en python es el OR) se tiene que cumplir una de las dos condiciones
+
+
+*/
