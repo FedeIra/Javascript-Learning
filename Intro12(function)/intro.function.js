7d41b3e22e111953ddc3a8303340fdb00a1b7f32
Federico Irarrazaval <fedeirar@gmail.com>
2022-05-07 01:42:19 -0300

    videp 22

--- Intro12(function)/intro.function.js
+++ Intro12(function)/intro.function.js
@@ -5,4 +5,15 @@ function nombre (parametros) {
 }
 
 nombre_de_la_funcion(); //esto es para llamarla
+
+
+PARAMETROS DE UNA FUNCION
+
+function suma (num1, num2) {
+  let resultado = num1+num2;
+}
+
+suma(5,7); //llamamos la funcion pasandole los dos parametros.
+
+//es el equivalente a decirl let num1 = 5 y let num2= 7
 */
