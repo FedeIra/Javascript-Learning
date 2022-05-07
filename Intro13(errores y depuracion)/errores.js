6a389a20e8e56467689cac8c6e0ce6ae61971712
Federico Irarrazaval <fedeirar@gmail.com>
2022-05-07 13:16:17 -0300

    video77

---
+++ Intro13(errores y depuracion)/errores.js
@@ -0,0 +1,12 @@
+/*
+Todos los navegadores abren una consola apretando f12
+
+Ejemplo, estan en googlechrome, apretass f12 y te muestra la consola. ahi podes ver los errores
+
+En la pestania element ves el codigo y en la pestania consola te dice el error. Hasta te indica en que linea esta el error
+Si apretas sobre el error te dirige al error.
+
+Hay que acordarse de refrescar la consola con f5 por si le agregue contenido. A veces no aparece el error y es pq tenes q apretar f5 para refrescar. Pasa especialmente con internet explorer.
+
+Te dice la fila y columna donde esta el error.
+*/
