d020378c50409073f8b26bb843e8845c584bda72
Federico Irarrazaval <fedeirar@gmail.com>
2022-05-07 12:16:50 -0300

    video 24

---
+++ Intro12(function)/intro.function y eventos.js
@@ -0,0 +1,61 @@
+/* FUNCIONES
+
+function nombre (parametros) {
+  //codigo a ejecutar;
+}
+
+nombre_de_la_funcion(); //esto es para llamarla
+
+
+PARAMETROS DE UNA FUNCION
+
+function suma (num1, num2) {
+  let resultado = num1+num2;
+}
+
+suma(5,7); //llamamos la funcion pasandole los dos parametros.
+
+es el equivalente a decirl let num1 = 5 y let num2= 7
+
+EVENTOS
+Los eventos: son desencadenantes de la accion. Cuando navegamos por internet hacemos acciones, click, pasar por encima, etc. Esto interactua con nosotros.
+
+El evento es un suceso q ocurre en un momento determinado. Ejemplo, hacer click, pasar el raton por encima de la imagen, etc. Ese evento desencadena una accion.
+
+Nosotros le decimos q cuando ocurra ese evento llame a una funcion en la cual pasa algo.
+
+Eventos -- descripcion ----------------------------------------- elementos par los que esta definido (<>)
+-------------------------------------------------------------------------------------------------------------------------
+onblur -- se desencadena cuando se deselecciona un elemento ---- <button>, <input>, <label>, <select>, <textarea>, <body>
+
+onchange -- deseleccionar un elemento que se ha modificado ---- <input>, <select>, <textarea>
+
+onclick -- pinchar y soltar el raton -------------------------- todos los elementos
+
+ondblclick -- pinchar dos veces seguidas con el raton----------- todos los elementos
+
+onfocus ---- seleccionar un elemento ---------------------------- <button>, <input>, <label>, <select>, <textarea>,<body>
+
+onkeydown ---pulsar una tecla (sin soltar) ---------------------- elementos de formulario y <body>
+
+onkeypress --- pulsar una tecla --------------------------------- elementos de formulario y <body>
+
+onkeyup ------ soltar una tecla pulsada ------------------------- elementos de formulario y <body>
+
+onload-------- la pagina se ha cargado completamente ------------<body>
+
+onmousedown----pulsar (sin soltar) un boton del rato ----------- todos los elementos
+
+onmousemove ---mover el raton -----------------------------------todos los elementos
+
+onmouseout---el raton "sale" del elemento(pasa por encima de otro elemento)--- todos los elementos
+
+onmouseover ---el raton "entra" en elemento(pasa por encima) -------todos los elementos
+
+onmouseup---soltar el boton que estaba pulsado en el raton -----------todos los elementos
+
+onreset----inicializar el formulario(borrar todos sus datos) ---------- <form>
+
+onresize---se ha modifiaado el tamanio de la ventana del navegador --- <body>
+
+*/
