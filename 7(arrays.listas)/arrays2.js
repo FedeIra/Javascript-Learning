95dfb21634cdd678b37d5bed63c63b93dc114f18
FedeIra <fedeirar@gmail.com>
2022-05-12 21:20:14 -0300

    organiceCarpetas.2

--- 7(arrays.listas)/arrays2.js
+++ 7(arrays.listas)/arrays2.js
@@ -1,4 +1,15 @@
-var array = ["toni", "emi", "franco", "jimmy", function() {console.log("hola")}, 5, [1,2,3,4], true];
+var array = [
+  "toni",
+  "emi",
+  "franco",
+  "jimmy",
+  function () {
+    console.log("hola");
+  },
+  5,
+  [1, 2, 3, 4],
+  true,
+];
 
 console.log(array[0]); // toni
 console.log(array[1]); // emi
@@ -10,18 +21,18 @@ console.log(array[4]()); // aca estoy invocando la funcion del array, para eso l
 
 //tambien puedo hacer cosas con el array:
 
-console.log(array[5] *3); // aca agarra el quinto array que tiene el valor de 5 y lo multiplica por 3
+console.log(array[5] * 3); // aca agarra el quinto array que tiene el valor de 5 y lo multiplica por 3
 
 console.log(array[1] + " q mal nombre!");
 
 // los strings a su vez son como si hubiera adentro un array
 console.log("mati"[2]); // aca le pido que lea el tercer (2) elemento del array q es string, es decir te tira: t
 
-console.log(array [0] [2]); // tambien puedo invocarla dentro de un array al array del string. 
+console.log(array[0][2]); // tambien puedo invocarla dentro de un array al array del string.
 
 console.log(array[0].length); // aca le pido el length del primer elemento del array, es decir: 4
 
-var arreglo = [1,2,3,4, "toni", [1,2,3,24]]; // el arreglo es una estructura de datos
+var arreglo = [1, 2, 3, 4, "toni", [1, 2, 3, 24]]; // el arreglo es una estructura de datos
 
 console.log(arreglo[0]); // los arrays empiezan a contar desde 0. 0 es siempre el primero.
 
@@ -41,4 +52,4 @@ lista_ejemplo.pop(); // el pop te saca el ultimo objeto de la lista
 var elemento_sacado_de_lista = lista_ejemplo.pop(); //en este caso la variable seria el ultimo objeto de la lista, "elemento 10" ya q sacamos el objeto 1 anteriormente
 console.log(lista_ejemplo);
 
-return lista_ejemplo.pop(); // con esto te saca el elemento pero te lo devuelve
+lista_ejemplo.pop(); // con esto te saca el elemento pero te lo devuelve
