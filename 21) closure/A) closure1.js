b20cae2201b1516a0c084e2b40efb4a304c962e0
Federico Irarrazaval <fedeirar@gmail.com>
2022-07-16 19:54:56 -0300

    closure

--- 21) closure/A) closure1.js
+++ 21) closure/A) closure1.js
@@ -193,3 +193,216 @@ var creaFuncion = function () {
 var arr = creaFuncion();
 arr[0](); // 3 sale un 3, qué esperaban ustedes??
 arr[1](); // 3
+
+/* CLOSURES:
+
+Necesitamos 3 ingredientes:
+
+1) Función escrita adentro de otra función (función anidada): */
+
+function crearContador0() {
+  return function incrementar0() {};
+}
+
+/*2) Variable en el scope de la función más grande y que la función de adentro la utilice de alguna manera: */
+
+function crearContador1() {
+  let contador1 = 0;
+  return function incrementar1() {
+    contador1 = contador1 + 1;
+    return contador1;
+  };
+}
+
+/*3) Invocar a la función interna desde otro scope, pero no desde el scope desde donde está escrita, sino desde otro scope. Como a una variable le podemos asignar un número o string, también le podemos asignar una función. Entonces podemos hacer que cuando llamemos a nuestra función más grande (crearContador) y engloba la de adentro esta nos va a devolver la función interna que podemos guardar en una variable para llamar más adelante. Ahora la función interna se ha convertida en un closure ya que al contener una referencia a una variable declarada en el scope de una función superior, es como el scope contuviera una variable y se generara un vínculo entre ellas. Desde el scope global no podemos acceder a la función interna incrementar(), sin embargo cuando invoquemos la función más grande, esta va a estar vinculada a la función local  y va a poder accederla normalmente: */
+
+function crearContador2() {
+  let contador2 = 0;
+  return function incrementar2() {
+    contador2 = contador2 + 1;
+    return contador2;
+  };
+}
+
+const contador3 = crearContador2();
+contador3();
+contador3();
+contador3();
+
+const contador4 = crearContador2();
+contador4();
+
+/* En resumen, las closure son funciones anidadas que recuerdan el conjunto de variables a las que podían acceder, por más que se las invoque desde otro lugar, desde otro scope. */
+
+/* USOS:
+Interactuar con este objeto pero protegiendo las variables internas.
+
+Podríamos agregar la funcionalidad para decrementar el contador y retornar contador.
+*/
+
+function crearContador() {
+  let contador = 0;
+
+  return {
+    incrementar: function () {
+      contador = contador + 1;
+      return contador;
+    },
+    decrementar: function () {
+      contador = contador - 1;
+      return contador;
+    },
+    obtenerValor: function () {
+      return contador;
+    },
+  };
+}
+
+const contador1 = crearContador();
+
+contador1.incrementar(); // 1
+contador1.incrementar(); // 2
+contador1.decrementar(); // 1
+contador1.obtenerValor(); // 1
+
+const contador2 =
+  crearContador(); /* El contador2 pertenece a un nuevo scope distinto al contador 1. */
+contador2.obtenerValor(); // 0
+
+/* Decimos que se protege el valor interno de la función porque no se lo puede cambiar desde afuera (sin acceder a la función interna): */
+
+contador1.contador = 3;
+contador1.obtenerValor(); // 1 /* El valor de contador interno sigue siendo uno a pesar de que lo intentamos cambiar desde afuera. */
+
+/* También sirven como fabrica de funciones. Ej: fabricar un contador que empiece en 10:
+
+Para esto usamos un párametro y las clausuras pueden acceder al contador como antes*/
+
+function crearContador(contador = 0) {
+  /* le pongo que por default arranque en 0 */
+  return {
+    incrementar: function () {
+      contador = contador + 1;
+      return contador;
+    },
+    decrementar: function () {
+      contador = contador - 1;
+      return contador;
+    },
+    obtenerValor: function () {
+      return contador;
+    },
+  };
+}
+
+const contador1 = crearContador(10);
+
+contador1.incrementar(); // 11
+
+/*%c esto hace que te tome los párametros del background y color*/
+
+function crearImpresoraDeMensajes(tipo, estilos) {
+  return function mensaje(str) {
+    console.log(`%c ${tipo}: ${str}`, estilos);
+  };
+}
+
+const error = crearImpresoraDeMensajes(
+  `Error`,
+  `background: red; color: white;`
+);
+
+const warning = crearImpresoraDeMensajes(
+  "Warn",
+  "background: orange; color: white;"
+);
+
+const exito = crearImpresoraDeMensajes(
+  "Éxito",
+  "background: green; color: white;"
+);
+
+console.log(
+  exito("Hola")
+); /* El párametro de la función mensaje (str) lo paso al invocar la función */ // Éxito: Hola
+
+// Puedo usar esta función para otras cosas:
+
+const holaFede = crearImpresoraDeMensajes(
+  "Fede es el mejor, por qué?",
+  "background: red; color: white, style: bold"
+);
+
+console.log(holaFede("simplemente porque lo és")); // Fede es el mejor, por qué?: simplemente porque lo és
+undefined;
+
+/*%c Si quiero que haya una constante dentro de las funciones internas en lugar de declararlo en la función padre y crear cada entorno de las funciones interas esta constanta (lo que ocupa espacio, especialmente si es un objeto), la puedo crear en el entorno global del que se va a agarrar esta función.*/
+
+const constanteGlobal =
+  "background: black; color: green;"; /*Aca se guarda x1 en el entorno global. Si lo declarara dentro de la función, se guardaría una vez por cada función interna, es decir x3. Ocuparía el triple de espacio. Le guardo un formato de background y color por defecto. En caso de que se complete luego, el nuevo sobreescribe este*/
+
+function crearImpresoraDeMensajes(tipo, estilos) {
+  return function mensaje(str) {
+    console.log(`%c ${tipo} : ${str}`, constanteGlobal + estilos);
+  };
+}
+
+const error3 = crearImpresoraDeMensajes(
+  `Error`,
+  `background: red; color: white;`
+);
+
+const error2 = crearImpresoraDeMensajes(`Error`);
+
+const warning3 = crearImpresoraDeMensajes(
+  "Warn",
+  "background: orange; color: white;"
+);
+
+const exito3 = crearImpresoraDeMensajes(
+  "Éxito",
+  "background: green; color: white;"
+);
+
+console.log(error2("Fede")); // Error : Fede (pero con otro color)
+
+/*%c Si quiero que haya una constante dentro de las funciones internas en lugar de declararlo en la función padre y crear cada entorno de las funciones interas esta constanta (lo que ocupa espacio, especialmente si es un objeto), la puedo crear en el entorno global del que se va a agarrar esta función.*/
+
+const constanteGlobal6 =
+  "background: black; color: green;"; /*Aca se guarda x1 en el entorno global. Si lo declarara dentro de la función, se guardaría una vez por cada función interna, es decir x3. Ocuparía el triple de espacio. Le guardo un formato de background y color por defecto. En caso de que se complete luego, el nuevo sobreescribe este*/
+
+function crearImpresoraDeMensajes(tipo, estilos) {
+  return function mensaje(str) {
+    console.log(`%c ${tipo} : ${str}`, constanteGlobal + estilos);
+  };
+}
+
+const error4 = crearImpresoraDeMensajes(
+  `Error`,
+  `background: red; color: white;`
+);
+
+const error5 = crearImpresoraDeMensajes(`Error`);
+
+const warning4 = crearImpresoraDeMensajes(
+  "Warn",
+  "background: orange; color: white;"
+);
+
+const exito4 = crearImpresoraDeMensajes(
+  "Éxito",
+  "background: green; color: white;"
+);
+
+/* console.log(exito("Hola")); */ /* El párametro de la función mensaje (str) lo paso al invocar la función */ // Éxito: Hola
+
+console.log(error5("Fede")); // Error : Fede (pero con otro color)
+// Puedo usar esta función para otras cosas:
+
+/* const holaFede = crearImpresoraDeMensajes(
+  "Fede es el mejor, por qué?",
+  "background: red; color: white, style: bold"
+);
+
+console.log(holaFede("simplemente porque lo és")); // Fede es el mejor, por qué?: simplemente porque lo és
+undefined */
