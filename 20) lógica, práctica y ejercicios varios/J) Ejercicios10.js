1cc5864cea039cf68ddb5e38ffe7cb58266a7ccb
cameronmcnz <you@example.com>
2022-08-28 13:37:01 -0300

    exercises 10

--- 20) lógica, práctica y ejercicios varios/J) Ejercicios10.js
+++ 20) lógica, práctica y ejercicios varios/J) Ejercicios10.js
@@ -43,3 +43,58 @@ function sumAll3(arr) {
 }
 
 sumAll3([1, 4]); // 10
+
+//-------------------------------------------------------
+function diffArray(arr1, arr2) {
+  let newArray = arr1;
+  arr1 = arr1.filter((element) => !arr2.includes(element));
+  arr2 = arr2.filter((element) => !newArray.includes(element));
+  newArray = [...arr1, ...arr2];
+  return newArray;
+}
+// diffArray(['diorite', 'andesite', 'grass', 'dirt', 'pink wool', 'dead shrub'],['diorite', 'andesite', 'grass', 'dirt', 'dead shrub']); // ["pink wool"]
+
+// diffArray(['andesite', 'grass', 'dirt', 'pink wool', 'dead shrub'], ['diorite', 'andesite', 'grass', 'dirt', 'dead shrub']); // ["diorite", "pink wool"]
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]); // 4
+diffArray(
+  ['andesite', 'grass', 'dirt', 'pink wool', 'dead shrub'],
+  ['diorite', 'andesite', 'grass', 'dirt', 'dead shrub']
+); // ["diorite", "pink wool"]
+diffArray([1, 'calf', 3, 'piglet'], [7, 'filly']); // ["calf", "piglet", "filly"]
+
+// Otras soluciones
+function diffArray(arr1, arr2) {
+  return arr1
+    .concat(arr2)
+    .filter((x) => !arr1.includes(x) || !arr2.includes(x));
+}
+
+// Otra solución:
+function diffArray(arr1, arr2) {
+  return [...diff(arr1, arr2), ...diff(arr2, arr1)];
+
+  function diff(a, b) {
+    return a.filter((item) => b.indexOf(item) === -1);
+  }
+}
+
+//-------------------------------------------------------
+
+function destroyer(arr) {
+  let allArguments = [];
+  for (let i = 1; i < arguments.length; i++) {
+    allArguments.push(arguments[i]);
+  }
+  return arguments[0].filter((element) => !allArguments.includes(element));
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3); // [1, 1]
+destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3); // [1, 5, 1]
+destroyer(['tree', 'hamburger', 53], 'tree', 53); // ["hamburger"]
+destroyer([3, 5, 1, 2, 2], 2, 3, 5); // [1]
+
+// Otra solución:
+function destroyer(arr, ...valsToRemove) {
+  return arr.filter((elem) => !valsToRemove.includes(elem));
+}
