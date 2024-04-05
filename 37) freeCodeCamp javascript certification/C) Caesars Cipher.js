ec5a8ec877c7ecc9d01fbd0e04205f8cb47057c1
Federico Irarrazaval <federico.irarrazaval@itglobers.com>
2024-04-05 15:40:01 -0300

    finished caesars cipher exercise

--- 37) freeCodeCamp javascript certification/C) Caesars Cipher.js
+++ 37) freeCodeCamp javascript certification/C) Caesars Cipher.js
@@ -11,10 +11,101 @@ All letters will be uppercase. Do not transform any non-alphabetic character (i.
 */
 
 function rot13(str) {
-  return str;
+  const alphabetx2 = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+
+  let stringToArray = str.split('');
+
+  let decipheredCode = [];
+
+  for (let index = 0; index < stringToArray.length; index++) {
+    if (alphabetx2.includes(stringToArray[index])) {
+      let indexAssignLetter = alphabetx2.indexOf(stringToArray[index]) + 13;
+      decipheredCode.push(alphabetx2[indexAssignLetter]);
+    } else {
+      decipheredCode.push(stringToArray[index]);
+    }
+  }
+
+  decipheredCode = decipheredCode.join('');
+
+  return decipheredCode;
 }
 
-rot13('SERR PBQR PNZC'); // FREE CODE CAMP
-rot13('SERR CVMMN!'); // FREE PIZZA!
-rot13('SERR YBIR?'); // FREE LOVE?
-rot13('GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.'); // THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+// rot13('SERR PBQR PNZC'); // FREE CODE CAMP
+// rot13('SERR CVMMN!'); // FREE PIZZA!
+// rot13('SERR YBIR?'); // FREE LOVE?
+// rot13('GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.'); // THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+
+// ----------------------------------------
+// Optimized solution:
+function optimizedRot13(str) {
+  // A function to shift a single character
+  function shiftChar(char) {
+    let charCode = char.charCodeAt(0);
+    // Check if the character is uppercase alphabetic
+    if (charCode >= 65 && charCode <= 90) {
+      // Shift the character code by 13, wrapping around if necessary
+      charCode = ((charCode - 65 + 13) % 26) + 65;
+    }
+    return String.fromCharCode(charCode);
+  }
+
+  // Transform each character of the input string
+  let result = str.split('').map(shiftChar).join('');
+  return result;
+}
+
+console.log(optimizedRot13('SERR PBQR PNZC'));
